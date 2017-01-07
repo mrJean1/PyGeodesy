b9945054a140bb53cf2da3e381120e8907bbd24f
mrJean1 <mrJean1@Gmail.com>
2017-01-07 15:02:44 -0500

    Updated Python code checkers to latest versions: pychok 17.01.07, pychecker 0.8.19, pyflakes 1.4.0, pycodestyle (formerly pep8) 2.2.0 (on 64-bit Python 2.7.13).

--- geodesy/__init__.py
+++ geodesy/__init__.py
@@ -5,9 +5,10 @@
 # spherical earth models using trigonometric and vector-based methods.
 
 # All modules have been checked statically with PyChecker, PyFlakes,
-# Pep8 and pychok* using Python 2.7.10 and 2.7.13 and tested with
-# 64-bit Python 2.7.10, 2.7.13, 3.5.2 and 3.6.0, but only on MacOS
-# 10.10 Yosemite, MacOS 10.11 El Capitan and MacOS 10.12.2 Sierra.
+# PyCodeStyle (formerly Pep8) and pychok* using Python 2.7.10 and
+# 2.7.13 and tested with 64-bit Python 2.7.10, 2.7.13, 3.5.2 and 3.6.0,
+# but only on MacOS 10.10 Yosemite, MacOS 10.11 El Capitan and MacOS
+# 10.12.2 Sierra.
 
 # Transcribed from JavaScript originals by (C) Chris Veness 2005-2016
 # and published under the same MIT Licence**.
@@ -51,7 +52,7 @@ VincentyError = ellipsoidalVincenty.VincentyError
 __all__ = ('ellipsoidalNvector', 'ellipsoidalVincenty',
            'sphericalNvector', 'sphericalTrigonometry',
            'VincentyError')  # extended below
-__version__ = '16.12.29'
+__version__ = '17.01.07'
 
 # lift all public constants, functions, etc.
 import datum as _datum, dms as _dms, mgrs as _mgrs, \
